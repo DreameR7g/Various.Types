0878910d6aaf1bacaf1a3794fc34b12a11c0fd78
Andrew Kernats <andrew_kernats@comcast.net>
2022-02-20 19:52:26 -0600

    mess with changes to and from json

--- main.go
+++ main.go
@@ -1,51 +1,73 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
 
 type Character struct {
 	// name
-	Name string
+	Name string `json:"name"`
 	// job
-	Job string
+	Job string `json:"job"`
 	// level
-	Level int
+	Level int `json:"level"`
 	// race
-	Race string
+	Race string `json:"race"`
 	// Current Date and Time
 	Date time.Time
 }
 
 func main() {
+	party := make(map[string]Character)
+
 	fmt.Println("Light Party")
-	fmt.Println(Character{
+	party["Dreamer"] = Character{
 		Name:  "Dreamer Rigorstorm",
 		Job:   "Monk",
 		Level: 90,
 		Race:  "Lalafell",
 		Date:  time.Now(),
-	})
-	fmt.Println(Character{
+	}
+	party["Siryu"] = Character{
 		Name:  "Siryu Unslay",
 		Job:   "Dark Knight",
 		Level: 90,
 		Race:  "Au'Ra",
 		Date:  time.Now(),
-	})
-	fmt.Println(Character{
+	}
+	party["James"] = Character{
 		Name:  "James Carmike",
 		Job:   "Sage",
 		Level: 90,
 		Race:  "Viera",
 		Date:  time.Now(),
-	})
-	fmt.Println(Character{
+	}
+	party["Kisa"] = Character{
 		Name:  "Kisa Rozen",
 		Job:   "Reaper",
 		Level: 90,
 		Race:  "Hyur",
 		Date:  time.Now(),
-	})
+	}
+	// for _, member := range party {
+	// 	fmt.Println(member)
+	// }
+
+	output, err := json.Marshal(party)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	var lightParty map[string]Character
+
+	err = json.Unmarshal(output, &lightParty)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	fmt.Println(lightParty["Siryu"])
+
 }
